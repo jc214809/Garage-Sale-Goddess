629ffa3f70ddc9d3b65971deb4421759aa06606f
Joel Jayson Clark <jc214809@ohio.edu>
2016-06-04 22:57:25 -0400

    added ability to add items

--- login/login.js
+++ login/login.js
@@ -1,4 +1,19 @@
-angular.module( 'sample')
-.controller( 'LoginCtrl', function ($scope) {
+angular.module('sample')
+  .controller('LoginCtrl', function($scope, $http, $location) {
+    // create a blank object to handle form data.
+    $scope.item = {};
+    // calling our submit function.
+    $scope.submitForm = function() {
+      // Posting data to php file
+      $http({
+          method: 'POST',
+          url: "http://localhost:8080/Garage-Sale-Goddess-API/addItem",
+          data: $scope.item //forms user object
+        })
+        .success(function(data) {
+          alert("added")
+          $location.path('/');
+        });
+    };
 
-});
+  });
