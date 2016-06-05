5cb1a1b59c7210ffb20e0a58f5fc31e1d523c5ef
Joel Jayson Clark <jc214809@ohio.edu>
2016-06-04 22:26:09 -0400

    add get items and delete items

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-angular.module( 'sample', [
-  'ngRoute',
-  'sample.home'
-])
-.config( function myAppConfig ( $routeProvider,  $httpProvider, $locationProvider) {
-  $routeProvider
-    .when( '/', {
-      controller: 'HomeCtrl',
-      templateUrl: 'home/home.html',
-      pageTitle: 'Homepage'
-    })
-    .when( '/login', {
-      controller: 'LoginCtrl',
-      templateUrl: 'login/login.html',
-      pageTitle: 'Login'
-    });
-})
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$routeChangeSuccess', function(e, nextRoute){
-    if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
-      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample' ;
-    }
-  });
-});
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
+angular.module('sample', [
+    'ngRoute',
+    'sample.home'
+  ])
+  .config(function myAppConfig($routeProvider, $httpProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/home.html',
+        pageTitle: 'Homepage'
+      })
+      .when('/login', {
+        controller: 'LoginCtrl',
+        templateUrl: 'login/login.html',
+        pageTitle: 'Login'
+      });
+  })
+  .controller('AppCtrl', function AppCtrl($scope, $location) {
+    $scope.$on('$routeChangeSuccess', function(e, nextRoute) {
+      if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
+        $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample';
+      }
+    });
+  });
--- home/home.js
+++ home/home.js
@@ -1,4 +1,28 @@
-angular.module( 'sample.home', [])
-.controller( 'HomeCtrl', function HomeController($scope) {
-
-});
+angular.module('sample.home', [])
+  .controller('HomeCtrl', function HomeController($scope, $http, $location) {
+    $scope.todos = [];
+    $http.get("http://localhost:8080/Garage-Sale-Goddess-API/getAllItems")
+      .success(function(data, status, headers, config) {
+        $scope.todos = data;
+        console.log($scope.todos);
+      }).error(function(data, status, headers, config) {
+        alert("error");
+      })
+    $scope.toggle = function(id) {
+      $("#details-" + id).toggle("slow");
+    };
+    $scope.getTotalTodos = function() {
+      return $scope.todos.length;
+    };
+    $scope.removeTask = function(id, index) {
+      $scope.item = {
+        itemId: id
+      }
+      $http.post("http://localhost:8080/Garage-Sale-Goddess-API/deleteItem", $scope.item).success(function(data, status) {
+        $scope.todos.remove(index);
+      })
+    }
+    $scope.addTodo = function() {
+      $location.path('/login');
+    };
+  });
