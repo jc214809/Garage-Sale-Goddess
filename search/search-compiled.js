d20be938f8bc83f8700cf281212a5b79cf8d7d04
Joel Jayson Clark <jc214809@ohio.edu>
2017-09-03 17:06:33 -0400

    removed unneeded string

--- search/search-compiled.js
+++ search/search-compiled.js
@@ -44379,44 +44379,25 @@ angular.module('sample.search', [])
   .controller('SearchCtrl', function SearchController($scope, $http, $location) {
 
     $scope.listings = [];
+    $scope.searchTerms = ['Garage Sale', 'Yard Sale', 'Moving Sale'];
     var craigslist = require('node-craigslist'),
       client = new craigslist.Client({
         city: 'columbus'
       });
-
-    client.search('Garage Sale').then((listings) => {
-      // play with listings here...
-      listings.forEach((listing) => client.details(listing)
-        .then((details) => {
-          $scope.listings.push(details);
-          console.log(details);
-          $scope.$apply();
-        }))
-    }).catch((err) => {
-      console.error(err);
-    });
-    client.search('Yard Sale').then((listings) => {
-      // play with listings here...
-      listings.forEach((listing) => client.details(listing)
-        .then((details) => {
-          $scope.listings.push(details);
-          console.log(details);
-          $scope.$apply();
-        }))
-    }).catch((err) => {
-      console.error(err);
-    });
-    client.search('Moving Sale').then((listings) => {
-      // play with listings here...
-      listings.forEach((listing) => client.details(listing)
-        .then((details) => {
-          $scope.listings.push(details);
-          console.log(details);
-          $scope.$apply();
-        }))
-    }).catch((err) => {
-      console.error(err);
-    });
+    for (var i = 0; i < $scope.searchTerms.length; i++) {
+      client.search($scope.searchTerms[i]).then((listings) => {
+        // play with listings here...
+        listings.forEach((listing) => client.details(listing)
+          .then((details) => {
+            details.description = details.description.replace("QR Code Link to This Post", "");
+            $scope.listings.push(details);
+            console.log(details);
+            $scope.$apply();
+          }))
+      }).catch((err) => {
+        console.error(err);
+      });
+    }
     $scope.checkDate = function(dates) {
       for (var i = 0; i < dates.length; i++) {
         var result = dates[i].split(" ");
@@ -44424,13 +44405,12 @@ angular.module('sample.search', [])
         var saleDate = new Date(date[0], date[1], date[2]);
         var today = new Date();
         var dateDiffDays = Math.ceil((Math.ceil(saleDate.getTime() - today.getTime())) / (1000 * 3600 * 24));
-        if (dateDiffDays >= -10 && dateDiffDays <= 10) {
+        if (dateDiffDays >= -3 && dateDiffDays <= 8) {
 
         } else {
           return false;
         }
       }
-      return true;
     };
   });
 },{"node-craigslist":50}]},{},[459]);
