4e6c86c186a469b000e451e763b003ddeb969a8a
Joel Jayson Clark <jc214809@ohio.edu>
2018-09-01 13:46:36 -0400

    working Prototype of Craigslist RSS Feeds being parsed

--- search/search.js
+++ search/search.js
@@ -1,34 +1,63 @@
-angular.module('sample.search', [])
-  .controller('SearchCtrl', function SearchController($scope, $http, $location) {
-    // $scope.listings = [];
-    // $scope.searchTerms = ['Garage Sale', 'Yard Sale', 'Moving Sale', 'Estate Sale'];
-    // var craigslist = require('node-craigslist'),
-    //   client = new craigslist.Client({
-    //     city: 'columbus'
-    //   });
-    // for (var i = 0; i < $scope.searchTerms.length; i++) {
-    //   client.search($scope.searchTerms[i]).then((listings) => {
-    //     listings.forEach((listing) => client.details(listing)
-    //       .then((details) => {
-    //         details.description = details.description.replace("QR Code Link to This Post", "");
-    //         $scope.listings.push(details);
-    //         console.log(details);
-    //         $scope.$apply();
-    //       }))
-    //   }).catch((err) => {
-    //     console.error(err);
-    //   });
-    // }
-    // $scope.checkDate = function(dates) {
-    //   for (var i = 0; i < dates.length; i++) {
-    //     var result = dates[i].split(" ");
-    //     var date = result[1].split("-");
-    //     var saleDate = new Date(date[0], date[1], date[2]);
-    //     var today = new Date();
-    //     var dateDiffDays = Math.ceil((Math.ceil(saleDate.getTime() - today.getTime())) / (1000 * 3600 * 24));
-    //     if (dateDiffDays >= -1 && dateDiffDays <= 8) {
+    angular.module('sample.search', [])
+      .controller('SearchCtrl', function SearchController($scope, $http, $location) {
+        $scope.zipcode = 43230;
+        $scope.miles = "20";
+        $scope.dates = ["2018-08-31", "2018-09-01", "2018-09-02", "2018-09-03"]
+        $scope.listings = [];
 
-    //     }
-    //   }
-    // };
-  });
+        $scope.doesListingExists = function(listings) {
+          for (var i = 0; i < $scope.listings.length; i++) {
+            if ($scope.listings[i].guid == listings.guid && $scope.listings[i].pubDate == listings.pubDate) {
+              return true;
+            }
+          }
+          return false;
+        }
+
+        $scope.getIndex = function(listings) {
+          for (var i = 0; i < $scope.listings.length; i++) {
+            if ($scope.listings[i].title == listings.title && $scope.listings[i].pubDate == listings.pubDate) {
+              return i;
+            }
+          }
+          return null;
+        }
+
+        $scope.getFeed = function(date) {
+          $.ajax({
+            url: 'https://api.rss2json.com/v1/api.json',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+              rss_url: 'https://columbus.craigslist.org/search/gms?format=rss&postal=' + $scope.zipcode + '&query=garage%20sale&sale_date=' + date + '&search_distance=' + $scope.miles,
+              api_key: 'kyetlmarpkb6xpukpnsnglefro8ct4xi8gbcv4qp', // put your api key here
+              count: 50
+            }
+          }).done(function(response) {
+            for (var i in response.items) {
+              if ($scope.doesListingExists(response.items[i])) {
+                $scope.listings[$scope.getIndex(response.items[i])].dates.push(date);
+              } else {
+                response.items[i].dates = [];
+                response.items[i].dates.push(date)
+                if (response.items[i].hasOwnProperty('title')) {
+                  $scope.listings.push(response.items[i]);
+                }
+              }
+              $scope.$apply()
+            }
+            //console.dir($scope.listings);
+          })
+        };
+        $scope.updateSearch = function() {
+          $scope.listings = [];
+          for (var i = $scope.dates.length - 1; i >= 0; i--) {
+            $scope.getFeed($scope.dates[i]);
+          }
+        }
+
+        for (var i = $scope.dates.length - 1; i >= 0; i--) {
+          $scope.getFeed($scope.dates[i]);
+        }
+
+      });
