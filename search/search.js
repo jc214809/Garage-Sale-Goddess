0a72bdf028ee8fee0104d6097680f6ccced2fb9f
Joel Jayson Clark <jc214809@ohio.edu>
2018-09-03 15:36:18 -0400

    unhid update search and added Keyword search

--- search/search.js
+++ search/search.js
@@ -4,6 +4,8 @@
         $scope.miles = "20";
         $scope.dates = []; //"2018-08-31", "2018-09-01", "2018-09-02", "2018-09-03"
         $scope.listings = [];
+        $scope.keywords = ["Garage Sale", "Yard Sale", "Community Sale", "Community Garage Sale", "Moving Sale", "Barn Sale"];
+        $scope.secondaryKeywords = ["Tag Sale", "Estate Sale", "Rummage Sale"];
         var FRIDAY = 5;
 
         $scope.doesListingExists = function(listings) {
@@ -25,22 +27,27 @@
         }
 
         $scope.getFeed = function(date) {
-          $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20'https%3A%2F%2Fcolumbus.craigslist.org%2Fsearch%2Fgms%3Fformat%3Drss%26postal%3D" + $scope.zipcode + "%26query%3Dgarage%2520sale%26sale_date%3D" + date + "%26search_distance%3D" + $scope.miles + "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
-            .success(function(response, status) {
-              var listings = response.query.results.RDF.item;
-              for (var i in response.query.results.RDF.item) {
-                if ($scope.doesListingExists(listings[i])) {
-                  $scope.listings[$scope.getIndex(listings[i])].saleDates.push(date);
-                } else {
-                  listings[i].saleDates = [];
-                  listings[i].saleDates.push(date);
-                  if (listings[i].hasOwnProperty('title')) {
-                    $scope.scrubTitle(listings[i])
-                    $scope.listings.push(listings[i]);
+          for (var k = 0; k < $scope.keywords.length; k++) {
+            $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20'https%3A%2F%2Fcolumbus.craigslist.org%2Fsearch%2Fgms%3Fformat%3Drss%26postal%3D" + $scope.zipcode + "%26query%3D" + $scope.keywords[k].replace(/ /g, "%2520") + "%26sale_date%3D" + date + "%26search_distance%3D" + $scope.miles + "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+              .success(function(response, status) {
+                var listings = response.query.results.RDF.item;
+                for (var i in response.query.results.RDF.item) {
+                  if ($scope.doesListingExists(listings[i])) {
+                    var index = $scope.getIndex(listings[i]);
+                    if (!$scope.listings[index].saleDates.contains(date)) {
+                      $scope.listings[index].saleDates.push(date);
+                    }
+                  } else {
+                    listings[i].saleDates = [];
+                    listings[i].saleDates.push(date);
+                    if (listings[i].hasOwnProperty('title')) {
+                      $scope.scrubTitle(listings[i])
+                      $scope.listings.push(listings[i]);
+                    }
                   }
                 }
-              }
-            });
+              });
+          }
         };
         $scope.updateSearch = function() {
           $scope.listings = [];
